b21fdf71aa8bdac7aabbe2854b96145b83680906
Mike MikeScott8 Hamilton <mike@mikescott8.net>
2014-08-12 13:51:05 -0700

    added opml lib. added test api controller

---
+++ DisneyPodcastDirectory/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using DisneyPodcastDirectory.Helper;
+using FeedLister;
+
+namespace DisneyPodcastDirectory.Controllers
+{
+    public class TestController : ApiController
+    {
+        public object Get()
+        {
+            var opmlDocumentPath = Path.Combine(
+                AppDomain.CurrentDomain.GetData("DataDirectory").ToString(),
+                "ExtinctPodcastsdata.xml");
+            var opmlDocument = OpmlDocument.Load(opmlDocumentPath);
+
+            var cats = new List<string>();
+
+            cats.AddOutlines(string.Empty, opmlDocument.Body.Outlines);
+
+            return cats.ToArray();
+        }
+
+    }
+}
---
+++ DisneyPodcastDirectory/Helper/OutlineListExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using FeedLister;
+
+namespace DisneyPodcastDirectory.Helper
+{
+    public static class OutlineListExtensions
+    {
+        public static void AddOutline(this List<string> cats, string fillText, Outline outline)
+        {
+            var sb = new StringBuilder(fillText);
+            sb.Append(outline.Text);
+            switch (outline.Type)
+            {
+                case "rss":
+                    sb.Append(";podcast feed: " + outline.XmlUrl);
+                    break;
+                case "link":
+                    sb.Append(";html url: " + outline.Url);
+                    break;
+            }
+
+            cats.Add(sb.ToString());
+        }
+
+        public static void AddOutlines(this List<string> cats, string fillText, Outline[] outlines)
+        {
+            if (outlines != null)
+            {
+                foreach (var outline in outlines)
+                {
+                    //AddOutlineToList(cats, fillText, outline);
+                    cats.AddOutline(fillText, outline);
+
+                    if (outline.Outlines != null)
+                    {
+                        cats.AddOutlines(
+                            (string.IsNullOrWhiteSpace(fillText) ? ":-->" : fillText + fillText),
+                            outline.Outlines);
+                    }
+                }
+            }
+        }
+    }
+}
